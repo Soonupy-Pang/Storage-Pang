3d9fbf61fede83e2842225dce98918eb400f5760
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-05-08 21:34:29 +0900

    Initail commit

---
+++ src/main/java/com/example/storagepang/StoragepangApplication.java
@@ -0,0 +1,13 @@
+package com.example.storagepang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragepangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoragepangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storagepang/StoragepangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storagepang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoragepangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
